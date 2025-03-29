08c2d6f72f56a5879ec5dbd514fbc96e493320d3
Your Name <you@example.com>
2025-03-30 00:10:28 +0300

    Added home page

--- backend/sema_mama/urls.py
+++ backend/sema_mama/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the Sema Mama API!")
 
 urlpatterns = [
+    path("", home),
     path('admin/', admin.site.urls),
     path('api/mama/', include('mama.urls')),
     path('api/users/', include('users.urls')),
