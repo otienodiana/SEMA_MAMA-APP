640e7d23cac824aff4bb83ada20c4e7e79d4290f
Your Name <you@example.com>
2025-04-08 14:05:17 +0300

    commented out

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,13 +1,13 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+#from django.conf import settings
+#from django.conf.urls.static import static
+#from django.contrib import admin
+#from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/mama/', include('mama.urls')),
-    path('api/educational/', include('educational.urls')),
-    path('api/appointments/', include('appointments.urls')),
-]
+#urlpatterns = [
+  #  path('admin/', admin.site.urls),
+  #  path('api/mama/', include('mama.urls')),
+  #  path('api/educational/', include('educational.urls')),
+   # path('api/appointments/', include('appointments.urls')),
+#]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
